4a0211bf445b6282fcb71d8f3a025036f897bbba
dulcevaleriana <dulceguzmantaveras@gmail.com>
2022-06-02 13:45:19 -0400

    add .get() method to filter places by user id

--- backend-practice/routes/places-routes.js
+++ backend-practice/routes/places-routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+//BDA temporal
 const DUMMY_PLACES = [
     {
         id: 'pl',
@@ -14,16 +15,23 @@ const DUMMY_PLACES = [
         creator: 'url'
     }
 ]
-
-
+//get all element with principal id
 router.get('/:pId',(req,res,next)=>{
     const placeId = req.params.pId;
     const place = DUMMY_PLACES.find(p => {
         return p.id === placeId;
-    })
-    res.json({place: place});
+    });
+    res.json({place});
 
     console.log('GET REQUEST IN PLACES');
+})
+//get all element with the same user id
+router.get('/user/:uId',(req,res,next)=>{
+    const userId = req.params.uId;
+    const place = DUMMY_PLACES.find(p =>{
+        return p.creator === userId;
+    });
+    res.json({place})
 })
 
 module.exports = router;
