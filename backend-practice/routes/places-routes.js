d214f9261af56d73e97abc20675703311d67137a
dulcevaleriana <dulceguzmantaveras@gmail.com>
2022-06-02 12:34:30 -0400

    add a .get() method to places routers

--- backend-practice/routes/places-routes.js
+++ backend-practice/routes/places-routes.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req,res,next)=>{
-    console.log('GET REQUEST IN PLACES')
-    res.json({mesage: 'IT WORKS!!'});
+const DUMMY_PLACES = [
+    {
+        id: 'pl',
+        title: 'Empire State Building',
+        description: 'description description description description',
+        location:{
+            lat: 40.7484474,
+            lng: -73.9871516
+        },
+        address: 'address address address address',
+        creator: 'url'
+    }
+]
+
+
+router.get('/:pId',(req,res,next)=>{
+    const placeId = req.params.pId;
+    const place = DUMMY_PLACES.find(p => {
+        return p.id === placeId;
+    })
+    res.json({place: place});
+
+    console.log('GET REQUEST IN PLACES');
 })
 
 module.exports = router;
