6aaf0bba858ab02a1090f23d7d1cefa1966f148f
dulcevaleriana <dulceguzmantaveras@gmail.com>
2022-06-06 15:07:08 -0400

    start with users api get user, login and signup

--- backend-practice/controllers/users-controllers.js
+++ backend-practice/controllers/users-controllers.js
@@ -1,3 +1,7 @@
+const { v4: uuidv4 } = require('uuid');
+const httpError = require('../models/http-error');
+
+//BDA TEMPORAL
 let DUMMY_USERS = [
     {
         id:'ul',
@@ -7,13 +11,40 @@ let DUMMY_USERS = [
         image:'image.png'
     }
 ]
+//get all users
 const getUsers = (req,res,next) => {
     res.json({users: DUMMY_USERS})
 }
+//post a user
+const postUsers = (req,res,next) => {
+    const {
+        name,
+        email,
+        password,
+        image,
+    } = req.body;
+    const createUser = {
+        id: uuidv4(),
+        name,
+        email,
+        password,
+        image
+    };
 
-const postUsers = (req,res,next) => {}
-
-const login = (req,res,next) => {}
+    DUMMY_USERS.push(createUser);
+    res.status(200).json({user:createUser})
+}
+//login with a exist account
+const login = (req,res,next) => {
+    const {
+        email,
+        password
+    } = req.body;
+    const identifiedUser = DUMMY_USERS.find(u => u.email === email);
+    if(!identifiedUser){
+        throw new httpError("could not identify user, credentials seem to be wrong",401)
+    }
+}
 
 exports.getUsers = getUsers;
 exports.postUsers = postUsers;
