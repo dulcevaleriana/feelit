c390b33101c1b820dd65fa575806ded46de877ea
dulcevaleriana <dulceguzmantaveras@gmail.com>
2022-06-07 16:40:13 -0400

    complete paciente get paciente module

--- feelit-backend/controllers/paciente-controllers.js
+++ feelit-backend/controllers/paciente-controllers.js
@@ -5,11 +5,17 @@ const {validationResult} = require('express-validator');
 let DBA_PACIENTE = [
     {
         id:'dadafgsgfsdgrgf',
+        cedula:'402-2334268-0',
+        email:'juanOrtega@gmail.com',
+        password:'dsfdgds',
+        telefono:'849-654-9687',
+        name:'Juan Ortega',
+        status:true
     }
 ]
 //get all paciente
 const getAllPaciente = (req,res,next)=>{
-
+    res.json({DBA_PACIENTE})
 };
 //get paciente by id
 const getPacienteById = (req,res,next)=>{
