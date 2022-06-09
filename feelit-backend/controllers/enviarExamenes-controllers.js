1c9aef9dc81f5ce083cb84b83abb1427c34e6dc1
dulcevaleriana <dulceguzmantaveras@gmail.com>
2022-06-09 12:51:27 -0400

    created: all get method for module enviar examenes

--- feelit-backend/controllers/enviarExamenes-controllers.js
+++ feelit-backend/controllers/enviarExamenes-controllers.js
@@ -22,19 +22,47 @@ const getAllEnviarExamenes = (req,res,next)=>{
 };
 //get enviar examenes by id
 const getEnviarExamenesById = (req,res,next)=>{
+    const enviarExamenesId = req.params.eeId;
+    const getEnviarExamenesId = DBA_ENVIAR_EXAMENES.find(p => p.id === enviarExamenesId)
 
+    if(!getEnviarExamenesId){
+        throw new httpError('could not find any examn sended',404)
+    }
+
+    res.status(201).json({getEnviarExamenesId})
 };
 //get enviar examenes by status
 const getEnviarExamenesByStatus = (req,res,next) => {
+    const enviarExamenesStatus = req.params.ToF === 'true' ? true : false;
+    const getEnviarExamenesStatus = DBA_ENVIAR_EXAMENES.filter(p => p.status === enviarExamenesStatus);
 
+    if(getEnviarExamenesStatus < 1){
+        throw new httpError(`could not find any examn sended with status ${enviarExamenesStatus}`,404)
+    }
+
+    res.status(201).json({getEnviarExamenesStatus});
 }
 //get enviar examenes by doctor
 const getEnviarExamenesByDoctor = (req,res,next) => {
+    const doctorId = req.params.dId;
+    const getEnviarExamenesDoctor = DBA_ENVIAR_EXAMENES.filter(p => p.idDoctor === doctorId);
 
+    if(getEnviarExamenesDoctor < 1){
+        throw new httpError(`could not find any examn sended with this doctor`,404)
+    }
+
+    res.status(201).json({getEnviarExamenesDoctor})
 }
 //get enviar examenes by date
 const getEnviarExamenesByDate = (req,res,next) => {
+    const consultasRapidasDate = req.params.date;
+    const getEnviarExamenesDate = DBA_ENVIAR_EXAMENES.filter(p => p.dateCreated === consultasRapidasDate)
 
+    if(getEnviarExamenesDate < 1){
+        throw new httpError(`Could not find any with this date`,404)
+    }
+
+    res.status(201).json({getEnviarExamenesDate})
 }
 //post a: enviar examenes
 const postEnviarExamenes = (req,res,next)=>{
