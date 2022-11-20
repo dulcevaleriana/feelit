d17300ac20defe68482fdd9d32ed1db394feaa85
dulcevaleriana <dulceguzmantaveras@gmail.com>
2022-11-20 13:00:10 -0400

    continue adding element on  filter component

--- front/src/components/ConsultaRapidaComponent/FilterComponent.js
+++ front/src/components/ConsultaRapidaComponent/FilterComponent.js
@@ -4,6 +4,10 @@ import Box from '@mui/material/Box';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import OutlinedInput from '@mui/material/OutlinedInput';
+import BasicButtons from "../../components/UIElements/BasicButtons-MUI";
+import { faList, faTableCellsLarge } from '@fortawesome/free-solid-svg-icons';
+import CheckBox from "../UIElements/CheckBox";
+import DateTimeComponent from "../UIElements/DateTimeComponent";
 
 const filterArray = [
     {
@@ -21,24 +25,69 @@ const filterArray = [
 ]
 
 export default function FilterComponent(){
+
     return (
         <Box>
-            <h5>Mis citas</h5>
-            <FormControl>
-                <BasicSelect
-                    name="Filtrar por"
-                    filterArray={filterArray}
-                />
-            </FormControl>
-            <FormControl>
-                <InputLabel htmlFor="component-outlined">Escriba aquí</InputLabel>
-                <OutlinedInput
-                    id="component-outlined"
-                    // value={name}
-                    // onChange={handleChangeName}
-                    label="Escriba aquí"
-                />
-            </FormControl>
+            <div>
+                <h5>Mis citas</h5>
+                <FormControl>
+                    <BasicSelect
+                        name="Filtrar por"
+                        filterArray={filterArray}
+                    />
+                </FormControl>
+                <FormControl>
+                    <InputLabel htmlFor="component-outlined">Escriba aquí</InputLabel>
+                    <OutlinedInput
+                        id="component-outlined"
+                        // value={name}
+                        // onChange={handleChangeName}
+                        label="Escriba aquí"
+                    />
+                </FormControl>
+                <FormControl>
+                    <BasicButtons
+                        onClick={()=>{}}
+                        variantName="contained"
+                        buttonName={""}
+                        iconName={faTableCellsLarge}
+                    />
+                    <BasicButtons
+                        onClick={()=>{}}
+                        variantName="contained"
+                        buttonName={""}
+                        iconName={faList}
+                    />
+                </FormControl>
+            </div>
+            <div>
+                <FormControl>
+                    <CheckBox
+                        name="Solo consultas flash"
+                    />
+                    <CheckBox
+                        name="Solo citas"
+                    />
+                    <CheckBox
+                        name="Solo Resultados Enviados"
+                    />
+                </FormControl>
+                <FormControl>
+                    <DateTimeComponent/>
+                </FormControl>
+                <FormControl>
+                    <BasicButtons
+                        onClick={()=>{}}
+                        variantName="contained"
+                        buttonName={"Activo"}
+                    />
+                    <BasicButtons
+                        onClick={()=>{}}
+                        variantName="contained"
+                        buttonName={"Inactivo"}
+                    />
+                </FormControl>
+            </div>
         </Box>
     )
 }
