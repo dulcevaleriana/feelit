230f66ea0e894387096cd8a84a40d930473dce47
dulcevaleriana <dulceguzmantaveras@gmail.com>
2022-11-26 12:44:57 -0400

    start component upload documents

--- front/src/components/EnviarResultados/UploadButtons.js
+++ front/src/components/EnviarResultados/UploadButtons.js
@@ -1,30 +1,43 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import PhotoCamera from '@mui/icons-material/PhotoCamera';
+import React, { useState, useRef } from 'react';
 import Stack from '@mui/material/Stack';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFileImport, faFilePdf } from '@fortawesome/free-solid-svg-icons';
 
-export default function UploadButtons() {
-    const cardFile = (<div>ADD FILE WORK</div>)
-    let arrayFileUpload = []
+export default function UploadButtons(props) {
+    const cardFile = "ADD-FILE-WORK.pdf"
+    const [arrayFileUpload, setArrayFileUpload] = useState([])
+    const filePicketRef = useRef()
 
     const addPreviewFileUpload = () => {
-        arrayFileUpload.push({
-            cardFileElement: cardFile
-        })
+        filePicketRef.current.click()
+        setArrayFileUpload(arr => [
+            ...arr,
+            { cardFileElement: cardFile}
+        ])
     }
     console.log("arrayFileUpload",arrayFileUpload)
     return (
         <Stack direction="row" alignItems="center" spacing={2} className="class-UploadButtons">
             <div>
                 {arrayFileUpload.map((index, key)=>(
-                    <div key={key}>{index.cardFileElement}</div>
+                    <div key={key}>
+                        <FontAwesomeIcon icon={faFilePdf} size="lg"  />
+                        <h4>{index.cardFileElement}</h4>
+                    </div>
                 ))}
             </div>
-            <IconButton color="primary" aria-label="upload picture" component="label" onClick={addPreviewFileUpload}>
-                <input hidden accept="image/*" type="file" />
-                <PhotoCamera /> <p></p>
-            </IconButton>
+            <input
+                id={props.id}
+                type="file"
+                ref={filePicketRef}
+                style={{display:'none'}}
+                accept=".jpg,.png,.jpeg,.pdf"
+                // onChange={pickerHandle}
+            />
+            <button type='button' onClick={addPreviewFileUpload}>
+                <FontAwesomeIcon icon={faFileImport} size="lg"  />
+                <h4>Click aquí o arrastrar aquí para agregar documentos a enviar</h4>
+            </button>
         </Stack>
     );
 }
