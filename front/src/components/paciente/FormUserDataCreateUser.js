a1b8981971f2edb0c9e83dbc34448b428be7b499
dulcevaleriana <dulceguzmantaveras@gmail.com>
2022-12-03 13:59:03 -0400

    first step fetch on create doctor or paciente

--- front/src/components/paciente/FormUserDataCreateUser.js
+++ front/src/components/paciente/FormUserDataCreateUser.js
@@ -1,107 +1,23 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import FormControl from '@mui/material/FormControl';
-import InputLabel from '@mui/material/InputLabel';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import BasicSelect from '../UIElements/BasicSelect';
+import Input from '../../components/UIElements/InputComponent';
 
 export default function FormUserDataCreateUser(props) {
-    const [name, setName] = React.useState('Escriba aquí su nombre');
-    const [telefono, setTelefono] = React.useState('Escriba aquí su teléfono');
-    const [correo, setCorreo] = React.useState('Escriba aquí su correo');
-
-    const handleChangeName = (event) => {
-        setName(event.target.value);
-    };
-
-    const handleChangeTelefono = (event) => {
-        setTelefono(event.target.value);
-    };
-
-    const handleChangeCorreo = (event) => {
-        setCorreo(event.target.value);
-    };
-
-    const DUMfilterArray = [
-        {
-            value:10,
-            name:"nombre especialidad"
-        },
-        {
-            value:20,
-            name:"nombre especialidad"
-        },
-        {
-            value:30,
-            name:"nombre especialidad"
-        },
-        {
-            value:40,
-            name:"nombre especialidad"
-        },
-        {
-            value:50,
-            name:"nombre especialidad"
-        },
-        {
-            value:60,
-            name:"nombre especialidad"
-        },
-    ]
 
     return <Box>
-        <FormControl>
-            <InputLabel htmlFor="component-outlined">Nombre</InputLabel>
-            <OutlinedInput
-            id="component-outlined"
-            value={name}
-            onChange={handleChangeName}
-            label="Nombre"
+        {props.arrayInputs.map((index, key)=>(
+            <Input
+                key={key}
+                element={index.element}
+                id={index.id}
+                type={index.type}
+                label={index.label}
+                validators={index.validators}
+                errorText={index.errorText}
+                onInput={()=> index.onInput}
+                filterArray={index.filterArray ?? index.filterArray}
             />
-        </FormControl>
-        <FormControl>
-            <InputLabel htmlFor="component-outlined">Teléfono</InputLabel>
-            <OutlinedInput
-            id="component-outlined"
-            value={telefono}
-            onChange={handleChangeTelefono}
-            label="Telefono"
-            />
-        </FormControl>
-        <FormControl>
-            <InputLabel htmlFor="component-outlined">Correo</InputLabel>
-            <OutlinedInput
-            id="component-outlined"
-            value={correo}
-            onChange={handleChangeCorreo}
-            label="Correo"
-            />
-        </FormControl>
-        <FormControl>
-            <InputLabel htmlFor="component-outlined">Contraseña</InputLabel>
-            <OutlinedInput
-            id="component-outlined"
-            value={correo}
-            onChange={handleChangeCorreo}
-            label="Correo"
-            />
-        </FormControl>
-        <FormControl>
-            <InputLabel htmlFor="component-outlined">Teléfono</InputLabel>
-            <OutlinedInput
-            id="component-outlined"
-            value={correo}
-            onChange={handleChangeCorreo}
-            label="Correo"
-            />
-        </FormControl>
-        {!(props.pacienteOrDoctor) && <FormControl>
-            <label>Elige tu medico de preferencia, fecha y hora de la cita</label>
-            <BasicSelect
-                name="Filtrar por especialidad"
-                filterArray={DUMfilterArray}
-            />
-        </FormControl>}
+        ))}
         <h5>* Al llenar estos campos esta de acuerdo con crear una cuenta para tener su cita</h5>
     </Box>
 }
--- front/src/pages/paciente/CreateUserOrDoctor.js
+++ front/src/pages/paciente/CreateUserOrDoctor.js
@@ -6,29 +6,118 @@ import InputLabel from '@mui/material/InputLabel';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import FormUserDataCreateUser from "../../components/paciente/FormUserDataCreateUser";
 import AddDayAndTimeWork from "../../components/paciente/AddDayAndTimeWork";
-
+import { useForm } from "../../shared/hooks/form-hook";
 
 export default function CreateUserOrDoctor() {
     const [step, setStep] = useState(true);
     const [name ] = useState("")
     // setName
 
+    const [formState, inputHandler, setFormData] = useForm(
+        {
+            name: {
+                value: '',
+                isValid: false
+            },
+            password: {
+                value: '',
+                isValid: false
+            },
+            cedula: {
+                value: '',
+                isValid: false
+            },
+            email: {
+                value: '',
+                isValid: false
+            },
+            telefono: {
+                value: '',
+                isValid: false
+            }
+        },
+        false
+    );
+
+    console.log("formState",formState)
+
+    const switchDoctorOrPacient = (boolean) => {
+        if(step){
+            setFormData(
+                {
+                    name: {
+                        value: '',
+                        isValid: false
+                    },
+                    password: {
+                        value: '',
+                        isValid: false
+                    },
+                    cedula: {
+                        value: '',
+                        isValid: false
+                    },
+                    email: {
+                        value: '',
+                        isValid: false
+                    },
+                    telefono: {
+                        value: '',
+                        isValid: false
+                    },
+                    specialty: {
+                        value: '',
+                        isValid: false
+                    }
+                },
+                false
+            )
+        } else{
+            setFormData(
+                {
+                    name: {
+                        value: '',
+                        isValid: false
+                    },
+                    password: {
+                        value: '',
+                        isValid: false
+                    },
+                    cedula: {
+                        value: '',
+                        isValid: false
+                    },
+                    email: {
+                        value: '',
+                        isValid: false
+                    },
+                    telefono: {
+                        value: '',
+                        isValid: false
+                    }
+                },
+                false
+            )
+        }
+        setStep(boolean)
+    }
+
     useEffect(()=>{
         localStorage.removeItem("stepLS")
     },[step])
 
-    return <div className={step ? "class-CreateUser" : "class-CreateUser class-CreateDoctor"}>
+    return <form className={step ? "class-CreateUser" : "class-CreateUser class-CreateDoctor"}>
         <span>
             <FormControl>
                 <label>Elige tu medico de preferencia, fecha y hora de la cita</label>
                 <BasicButtons
-                    onClick={()=>setStep(true)}
+                    onClick={()=> switchDoctorOrPacient(true)}
                     variantName="contained"
                     buttonName="Paciente"
                     className=""
                 />
                 <BasicButtons
-                    onClick={()=>setStep(false)}
+                    onClick={()=> switchDoctorOrPacient(false)}
                     variantName="contained"
                     buttonName="Doctor/a"
                     className=""
@@ -58,7 +147,138 @@ export default function CreateUserOrDoctor() {
                 />
             </FormControl>
         </span>
-        <FormUserDataCreateUser pacienteOrDoctor={step}/>
+        <FormUserDataCreateUser
+            arrayInputs={step ? [
+                {
+                    element:"input",
+                    id:"Nombre",
+                    type:"text",
+                    label:"Nombre",
+                    validators:[],
+                    errorText:"Please enter a valid Nombre.",
+                    onInput:{inputHandler}
+                },
+                {
+                    element:"input",
+                    id:"Cédula",
+                    type:"number",
+                    label:"Cédula",
+                    validators:[],
+                    errorText:"Please enter a valid Cédula.",
+                    onInput:{inputHandler}
+                },
+                {
+                    element:"input",
+                    id:"Teléfono",
+                    type:"tel",
+                    label:"Teléfono",
+                    validators:[],
+                    errorText:"Please enter a valid Teléfono.",
+                    onInput:{inputHandler}
+                },
+                {
+                    element:"input",
+                    id:"Correo",
+                    type:"email",
+                    label:"Correo",
+                    validators:[],
+                    errorText:"Please enter a valid Correo.",
+                    onInput:{inputHandler}
+                },
+                {
+                    element:"input",
+                    id:"Contraseña",
+                    type:"password",
+                    label:"Contraseña",
+                    validators:[],
+                    errorText:"Please enter a valid Contraseña.",
+                    onInput:{inputHandler}
+                }
+            ]
+            :
+            [
+                {
+                    element:"input",
+                    id:"Nombre",
+                    type:"text",
+                    label:"Nombre",
+                    validators:[],
+                    errorText:"Please enter a valid Nombre.",
+                    onInput:{inputHandler}
+                },
+                {
+                    element:"input",
+                    id:"Cédula",
+                    type:"number",
+                    label:"Cédula",
+                    validators:[],
+                    errorText:"Please enter a valid Cédula.",
+                    onInput:{inputHandler}
+                },
+                {
+                    element:"input",
+                    id:"Teléfono",
+                    type:"tel",
+                    label:"Teléfono",
+                    validators:[],
+                    errorText:"Please enter a valid Teléfono.",
+                    onInput:{inputHandler}
+                },
+                {
+                    element:"input",
+                    id:"Correo",
+                    type:"email",
+                    label:"Correo",
+                    validators:[],
+                    errorText:"Please enter a valid Correo.",
+                    onInput:{inputHandler}
+                },
+                {
+                    element:"input",
+                    id:"Contraseña",
+                    type:"password",
+                    label:"Contraseña",
+                    validators:[],
+                    errorText:"Please enter a valid Contraseña.",
+                    onInput:{inputHandler}
+                },
+                {
+                    element:"select",
+                    id:"Especialidad",
+                    type:"select",
+                    label:"Especialidad",
+                    validators:[],
+                    errorText:"Please enter a valid Especialidad.",
+                    onInput:{inputHandler},
+                    filterArray:[
+                        {
+                            value:10,
+                            name:"nombre especialidad"
+                        },
+                        {
+                            value:20,
+                            name:"nombre especialidad"
+                        },
+                        {
+                            value:30,
+                            name:"nombre especialidad"
+                        },
+                        {
+                            value:40,
+                            name:"nombre especialidad"
+                        },
+                        {
+                            value:50,
+                            name:"nombre especialidad"
+                        },
+                        {
+                            value:60,
+                            name:"nombre especialidad"
+                        },
+                    ]
+                }
+            ]}
+        />
         {!step && <AddDayAndTimeWork/>}
         <BasicButtons
             onClick={()=>{}}
@@ -66,5 +286,5 @@ export default function CreateUserOrDoctor() {
             buttonName="Listo"
             iconName={faCheck}
         />
-    </div>
+    </form>
 }
