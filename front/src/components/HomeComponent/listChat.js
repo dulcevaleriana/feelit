4a94d95647d2f28e24a80b9881785655e51a5935
dulcevaleriana <dulceguzmantaveras@gmail.com>
2023-01-28 13:08:23 -0400

    try to condition paciente's list

--- front/src/components/HomeComponent/listChat.js
+++ front/src/components/HomeComponent/listChat.js
@@ -10,22 +10,47 @@ export default function ListChat(props){
     const [getList, setGetList] = useState(null)
     const [getSecondList, setGetSecondList] = useState(null)
 
+    const GetDoctorDataFunction = (idDoctor,data,key) => {
+        const [getResponse, setGetResponse] = useState(null)
+
+        useEffect(()=>{
+            const fetchData = async () => {
+                let response = await sendRequest(process.env.REACT_APP_ + 'doctor/' + idDoctor)
+                setGetResponse(response)
+            }
+            fetchData()
+
+            
+        },[idDoctor])
+
+        console.log({idDoctor})
+        console.log({getResponse})
+
+        return <ActionAreaCard
+            key={key}
+            img={getResponse?.img}
+            name={getResponse?.name}
+            specialty={getResponse?.specialty}
+            isLoggedIn={auth.isLoggedIn}
+            onClick={()=>props.onClick(data)}
+        />
+    }
+
     useEffect(()=>{
         const getUserFunction = async () => {
             const response = await sendRequest(process.env.REACT_APP_ + 'doctor/')
             setGetList(response)
 
             if(auth.rol === "638f3ddd1af87455b52cf7d7"){
-                const response = await sendRequest(process.env.REACT_APP_ + 'paciente/' + auth.userId);
-                setGetSecondList([
-                    ...response.getPacienteById.agendarCita ,
-                    ...response.getPacienteById.consultaRapida ,
-                    ...response.getPacienteById.enviarExamenes
-                ])
+                const response = await sendRequest(process.env.REACT_APP_ + 'paciente/getAllPacienteServices/' + auth.userId);
+                setGetSecondList(response)
             }
         }
         getUserFunction()
+        // eslint-disable-next-line
     },[sendRequest, auth.rol, auth.userId, auth.isLoggedIn])
+
+    console.log({getSecondList})
 
     return <div>
         <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
@@ -33,7 +58,11 @@ export default function ListChat(props){
         </Typography>
 
         { auth.rol === "638f3ddd1af87455b52cf7d7" ? <div>
-            {getSecondList && getSecondList.length === 0 ? "Ahora mismo no tienes ningun chat pendiente, inicia un chat con algun medico aqui" : "My Doctor list"}
+            {getSecondList &&
+                getSecondList.length === 0 ?
+                "Ahora mismo no tienes ningun chat pendiente, inicia un chat con algun medico aqui"
+                :
+                getSecondList?.getAllServices?.map((index, key) => <GetDoctorDataFunction idDoctor={index.idDoctor} key={key} data={index}/>)}
         </div> : auth.rol === "638f3dc51af87455b52cf7d4" ? <div>
             My pacient list
         </div> : null}
