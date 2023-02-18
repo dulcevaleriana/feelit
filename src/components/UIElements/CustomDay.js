2df3c4fb9e6741e10830205ced497bc70a8a10d4
dulcevaleriana <dulceguzmantaveras@gmail.com>
2023-02-18 19:55:54 -0400

    continue with calendar filter: filter weekends

--- src/components/UIElements/CustomDay.js
+++ src/components/UIElements/CustomDay.js
@@ -1,39 +1,50 @@
-import * as React from 'react';
+import React, { useEffect } from 'react';
 import dayjs from 'dayjs';
+import TextField from '@mui/material/TextField';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { CalendarPicker } from '@mui/x-date-pickers/CalendarPicker';
+import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
 import GetTodayDate from "../../shared/util/getTodayDate";
 
 export default function CustomDay(props) {
   const {actualDay} = GetTodayDate();
   const [date, setDate] = React.useState(dayjs(actualDay));
 
-  const filterDayNotAvaiable = (day) => {
-    console.log({horarioDoctor:props.horarioDoctor})
+  useEffect(()=>{
+    const filterDayNotAvaiable = (day) => {
+      console.log({horarioDoctor:props.horarioDoctor})
 
-    let newArrayHorario = props.horarioDoctor.map(data => {
-      return {
-        // entrada: new Date(Date.parse(JSON.parse(data.entrada))),
-        // salida: new Date(Date.parse(JSON.parse(data.salida)))
-        entrada: Date.parse(JSON.parse(data.entrada)),
-        salida: Date.parse(JSON.parse(data.salida))
-      }
-    })
-    console.log({newArrayHorario})
-    // console.log({date})
-    return true
-  }
+      let newArrayHorario = props.horarioDoctor.map(data => {
+        return {
+          // entrada: new Date(Date.parse(JSON.parse(data.entrada))),
+          // salida: new Date(Date.parse(JSON.parse(data.salida)))
+          entrada: Date.parse(JSON.parse(data.entrada)),
+          salida: Date.parse(JSON.parse(data.salida))
+        }
+      })
+      console.log({newArrayHorario})
+      console.log({day})
+      return true
+    }
+    filterDayNotAvaiable()
+  },[])
 
-  return <LocalizationProvider dateAdapter={AdapterDayjs}>
-    <div>
-      <h5>Selecciona una fecha</h5>
-      <CalendarPicker
-        date={date}
-        onChange={(newDate) => setDate(newDate)}
+  const isWeekend = (date) => {
+    const day = date.day();
+    return day === 0 || day === 6;
+  };
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <StaticDatePicker
+        displayStaticWrapperAs="desktop"
+        openTo="day"
+        value={date}
+        shouldDisableDate={isWeekend}
+        onChange={(newValue) => setDate(newValue)}
+        renderInput={(params) => <TextField {...params} />}
         disablePast={true}
-        shouldDisableDate={(day) => filterDayNotAvaiable(day)}
       />
-    </div>
-  </LocalizationProvider>
+    </LocalizationProvider>
+  );
 }
