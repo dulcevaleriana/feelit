12dc9d86e592ce66f94a1a33e9fcab6153b99346
dulcevaleriana <dulceguzmantaveras@gmail.com>
2023-02-19 13:42:51 -0400

    continue filter time

--- src/components/AgendarCita/TimeAvaiable.js
+++ src/components/AgendarCita/TimeAvaiable.js
@@ -9,11 +9,27 @@ export default function TimeAvaiable(props){
             let newTimeFormatArray = props.horarioDoctor.map(data => {
                 let getEntrada = new Date(Date.parse(JSON.parse(data.entrada)))
                 let getSalida = new Date(Date.parse(JSON.parse(data.salida)))
+                let newFormatEntrada = `${getEntrada.getHours()}:${getEntrada.getMinutes()}`
+                let newFormatSalida = `${getSalida.getHours()}:${getSalida.getMinutes()}`
+                const times = [];
+
+                function addMinutes(time, minutes) {
+                    const [hours, mins] = time.split(':').map(Number);
+                    const totalMinutes = hours * 60 + mins + minutes;
+                    const newHours = Math.floor(totalMinutes / 60).toString().padStart(2, '0');
+                    const newMins = (totalMinutes % 60).toString().padStart(2, '0');
+                    return `${newHours}:${newMins}`;
+                }
+
+                for (let time = newFormatEntrada; time <= newFormatSalida; time = addMinutes(time, 60)) {
+                    times.push({ time });
+                }
 
                 return {
                     dia: data.dia,
-                    entrada: `${getEntrada.getHours()}:${getEntrada.getMinutes()}`,
-                    salida: `${getSalida.getHours()}:${getSalida.getMinutes()}`
+                    entrada: newFormatEntrada,
+                    salida: newFormatSalida,
+                    timesBetween:times
                 }
             })
             setFormatTime(newTimeFormatArray)
@@ -23,19 +39,14 @@ export default function TimeAvaiable(props){
 
     console.log({formatTime})
 
-    return <div>
+    return <div className="class-TimeAvaiable">
         <h5>Selecciona un Tiempos Disponibles</h5>
-        {formatTime.filter(day => day.dia === "Lun").map(data => <>
-            <BasicButtons
+        <div>
+            {formatTime.filter(day => day.dia === "Lun").map(data => data.timesBetween.map(index => <BasicButtons
                 onClick={()=>{}}
                 variantName="outlined"
-                buttonName={data.entrada}
-            />
-            <BasicButtons
-                onClick={()=>{}}
-                variantName="outlined"
-                buttonName={data.salida}
-            />
-        </>)}
+                buttonName={index.time}
+            />))}
+        </div>
     </div>
 }
